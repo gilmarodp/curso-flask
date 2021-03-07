4e9ddd0c1ee1341a53140f5657478b7925546a1c
Gilmar <gilmar.odp@gmail.com>
2021-03-07 14:16:29 -0300

    implementando o método PUT

--- resources/hotel.py
+++ resources/hotel.py
@@ -29,36 +29,48 @@ class Hoteis(Resource):
         return {'hoteis': hoteis}
     
 class Hotel(Resource):
-    def get(self, hotel_id):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+    
+    def find_hotel(hotel_id):
         for hotel in hoteis:
             if hotel['hotel_id'] == hotel_id:
                 return hotel
+        return None
+            
+    def get(self, hotel_id):
+        hotel = Hotel.find_hotel(hotel_id)
+        
+        if hotel is not None:
+            return hotel
             
         return {'message': 'Hotel not found'}, 404 #not found
     
     def post(self, hotel_id):
-        argumentos = reqparse.RequestParser()
-        argumentos.add_argument('nome')
-        argumentos.add_argument('estrelas')
-        argumentos.add_argument('diaria')
-        argumentos.add_argument('cidade')
+        dados = Hotel.argumentos.parse_args()
         
-        dados = argumentos.parse_args()
-        
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            'nome': dados['nome'],
-            'estrelas': dados['estrelas'],
-            'diaria': dados['diaria'],
-            'cidade': dados['cidade']
-        }
+        novo_hotel = { 'hotel_id': hotel_id, **dados }
         
         hoteis.append(novo_hotel)
         
         return novo_hotel, 200
     
     def put(self, hotel_id):
-        pass
+        
+        dados = Hotel.argumentos.parse_args()
+        hotel = Hotel.find_hotel(hotel_id)
+        
+        novo_hotel = { 'hotel_id': hotel_id, **dados }
+        
+        if hotel is not None:
+            hotel.update(novo_hotel)
+            return novo_hotel, 200 # ok
+        
+        hoteis.append(novo_hotel)
+        return novo_hotel, 201 # created
     
     def delete(self, hotel_id):
         pass
