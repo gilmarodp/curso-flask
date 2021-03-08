9b007664d97418ca78b6cde4f65b50aa94fee7b1
Gilmar <gilmar.odp@gmail.com>
2021-03-08 09:34:44 -0300

    criando banco de dados com SQLAlchemy

--- models/hotel.py
+++ models/hotel.py
@@ -1,11 +1,22 @@
-class HotelModel:
+from sql_alchemy import banco
+
+
+class HotelModel(banco.Model):
+    __tablename__ = 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    nome = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
+
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade):
         self.hotel_id = hotel_id
         self.nome = nome
         self.estrelas = estrelas
         self.diaria = diaria
         self.cidade = cidade
-        
+
     def json(self):
         return {
             'hotel_id': self.hotel_id,
